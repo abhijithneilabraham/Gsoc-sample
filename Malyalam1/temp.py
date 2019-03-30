25959234bf4cb1d7138bace90cc9f3090713c9a6
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-30 19:18:17 +0530

    indic import

---
+++ Malyalam1/temp.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 19:17:34 2019
+
+@author: abhijithneilabraham
+"""
+
+from indicnlp.normalize.indic_normalize import IndicNormalizerFactory
