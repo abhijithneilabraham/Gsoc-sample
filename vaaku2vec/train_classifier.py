0ab1f3746adb3fe4d5b1177c2dca76787e9e877a
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-04-01 22:52:27 +0530

    update

---
+++ vaaku2vec/train_classifier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 22:49:26 2019
+
+@author: abhijithneilabraham
+"""
+
+from fastai.text import *
+from fastai.lm_rnn import get_rnn_classifer
+import html
+from nltk import word_tokenize
