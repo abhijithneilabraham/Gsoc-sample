c469c7f5f7a88aa6429ccf72c470a11c1e8eee0a
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-04-04 22:47:28 +0530

    mlmorph

---
+++ examples/mlmorph1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  4 22:39:51 2019
+
+@author: abhijithneilabraham
+"""
+
+from mlmorph import Analyser
+analyser = Analyser()
+print(analyser.analyse("സ്വീകരിക്കപ്പെട്ട"))
+'''
+from mlmorph import Generator
+generator = Generator()
+print(generator.generate("കേരളം<np><genitive>"))
+'''
