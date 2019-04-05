70bcb577ae2c0c582d0f9fc0171e51886b35f46a
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-04-05 14:57:17 +0530

    updated other tryouts

---
+++ examples/suggest1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 02:40:42 2019
+
+@author: abhijithneilabraham
+"""
+
+from pattern.es import suggest
+print(suggest("parot"))
