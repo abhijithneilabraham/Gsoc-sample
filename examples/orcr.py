6b7087a061aefb63c37b7c1e56883522585a8b65
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-04-11 21:41:59 +0530

    updated twitter search

---
+++ examples/orcr.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 19:45:22 2019
+
+@author: abhijithneilabraham
+"""
+from google.cloud import vision
+'''
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+'''
--- examples/searchingtwitter.py
+++ examples/searchingtwitter.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr  5 03:36:13 2019
+Created on Thu Apr 11 21:40:32 2019
 
 @author: abhijithneilabraham
 """
+
 from pattern.web import Twitter, plaintext
-#from textblob import TextBlob
 twitter = Twitter(language='en') 
 for tweet in twitter.search('"Happiness"', cached=False):
     print(plaintext(tweet.text))
-    '''
-    txt=plaintext(tweet.text)
-    blob = TextBlob(txt)
-    nn=blob.noun_phrases
-    print(nn)
-    '''
-
